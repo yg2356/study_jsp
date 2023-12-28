506a1a285d4b5dd94ce266450a9d79ae67dd98c9
김윤구 <@DESKTOP-P0M3KSP>
2023-12-29 00:08:48 +0900

    2일차

--- edu/src/main/java/com/edu/test/FirstServlet.java
+++ edu/src/main/java/com/edu/test/FirstServlet.java
@@ -1,7 +1,19 @@
 package com.edu.test;
 
+import java.io.*;
+
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 
 public class FirstServlet extends HttpServlet {
-
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException{
+		System.out.println("init() 실해됨!");
+	}
+	
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException{
+		System.out.println("service() 실행됨!");
+	}
 }
