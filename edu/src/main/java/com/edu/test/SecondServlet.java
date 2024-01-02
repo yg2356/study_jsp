90eec91182b32bbd59e2e90da712415903b77cbe
김윤구 <@DESKTOP-P0M3KSP>
2024-01-02 23:09:44 +0900

    요청 응답 객체

--- edu/src/main/java/com/edu/test/SecondServlet.java
+++ edu/src/main/java/com/edu/test/SecondServlet.java
@@ -12,5 +12,11 @@ public class SecondServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 		System.out.println("SecondServlet!!");
+		
+		PrintWriter out = resp.getWriter();
+		out.print("<html><head><Title>Test</Title></head>");
+		out.print("<body><h1>have a nice day!!</h1></body>");
+		out.print("</html>");
+		out.close();
 	}
 }
