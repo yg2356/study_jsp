c6199d100e69f297f313a93e331745e73409f67c
김윤구 <@DESKTOP-P0M3KSP>
2024-01-07 23:00:07 +0900

    네트워크 정보

---
+++ edu/src/main/java/com/edu/test/NetInfoServlet.java
@@ -0,0 +1,31 @@
+package com.edu.test;
+
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/netInfo")
+public class NetInfoServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		res.setContentType("text/html;charset=EUC-KR");
+		PrintWriter out = res.getWriter();
+		out.print("<html>");
+		out.print("<head><title>Request 정보 출력 Servlet</title></head>");
+		out.print("<body>");
+		out.print("<h3>네트워크 관련 요청 정보</h3>");
+		out.print("Request Scheme : " + req.getScheme() + "<br/>");
+		out.print("Server Name : " + req.getServerName() + "<br/>");
+		out.print("Server Address : " + req.getLocalAddr() + "<br/>");
+		out.print("Server Port : " + req.getServerPort() + "<br/>");
+		out.print("Client Address : " + req.getRemoteAddr() + "<br/>");
+		out.print("Client Host : " + req.getRemoteHost() + "<br/>");
+		out.print("Client Port : " + req.getRemotePort() + "<br/>");
+		out.print("</body></html>");
+		out.close();
+	}
+}
